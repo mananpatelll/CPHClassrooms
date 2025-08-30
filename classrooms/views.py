dbc72ca933bc722b5b60005da5dd1bb8e88860bc
mananpatelll <mananpatel30.5.1@gmail.com>
2025-08-30 10:28:47 -0400

    Fix : No building object error

--- classrooms/views.py
+++ classrooms/views.py
@@ -2,12 +2,17 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Building, Classroom
 
+# classrooms/views.py
 from django.db.models import Count, Q
+
 def buildings_index(request):
-    buildings = (Building.objects
-                 .annotate(classrooms_count=Count('classrooms', filter=Q(classrooms__is_published=True)))
-                 .order_by("name"))
+    buildings = (
+        Building.objects
+        .annotate(published_room_count=Count('classrooms', filter=Q(classrooms__is_published=True)))
+        .order_by("name")
+    )
     return render(request, "classrooms/buildings.html", {"buildings": buildings})
+
 
 def classroom_list_by_building(request, slug):
     building = get_object_or_404(Building, slug=slug)
